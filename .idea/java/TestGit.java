90ec84f44ff7676b54794c7b1aa7dbac8d9ab446
wlx521 <2141423129@qq.com>
2024-09-19 18:35:00 +0800

    这是我的第二次测试了

---
+++ .idea/java/TestGit.java
@@ -0,0 +1,12 @@
+/**
+ * @ClassName TestGit
+ * @Description TODO
+ * @Author 沉默小吴
+ * @Date 2024/9/19 18:33
+ */
+
+public class TestGit {
+    public static void main(String[] args) {
+        System.out.println("打完测试2 了 elel");
+    }
+}
